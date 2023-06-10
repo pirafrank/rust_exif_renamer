9efe187ffbd2f422c2df9e00dc4169d16cbe41e8
Francesco Pira <dev@fpira.com>
2023-06-10 15:11:17 +0200

    first working version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::ffi::OsStr;
 use std::fs;
 use std::path::Path;
 use chrono::{DateTime, NaiveDateTime, Local, TimeZone};
@@ -16,6 +17,16 @@ fn create_datetime_from_string(date_string: &str) -> Option<DateTime<Local>> {
 
 fn format_date(date: DateTime<Local>, pattern: &str) -> String {
     date.format(pattern).to_string()
+}
+
+fn rename_file(formatted_date: &str, path: &Path, extension: &OsStr) {
+    let new_filename = format!("{}.{}", formatted_date, extension.to_string_lossy());
+    let new_path = path.with_file_name(new_filename);
+    if let Err(err) = std::fs::rename(&path, &new_path) {
+        println!("Failed to rename file: {}", err);
+    } else {
+        println!("{} renamed to: {}", path.display(), new_path.display());
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -49,13 +60,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         if let Ok(exif) = exifreader.read_from_container(&mut bufreader) {
                             println!("filename: {}", path.display());
                             for f in exif.fields() {
-                                if f.tag.to_string().to_lowercase().starts_with("date") {
+                                if f.tag.to_string().to_lowercase().starts_with("datetimeoriginal") {
                                     let date_string = f.display_value().to_string();
-                                    println!("{} {} {}",
-                                        f.tag, f.ifd_num, date_string);
+                                    println!("{}: {}",
+                                        f.tag, date_string);
                                     let current_date = create_datetime_from_string(&date_string);
                                     let formatted_date = format_date(current_date.unwrap(), pattern);
                                     println!("Formatted date: {}", formatted_date);
+                                    rename_file(&formatted_date, &path, &extension);
+                                    continue;
                                 }
                             }
                             println!("");
