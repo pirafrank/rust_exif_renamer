2deb46119a0355e869b863d91623b6bc82b4d2dd
Francesco Pira <dev@fpira.com>
2023-06-10 14:51:10 +0200

    date formatter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use std::env;
 use std::fs;
 use std::path::Path;
+use chrono::{DateTime, NaiveDateTime, Local, TimeZone};
+
+
+fn create_datetime_from_string(date_string: &str) -> Option<DateTime<Local>> {
+    let format = "%Y-%m-%d %H:%M:%S";
+    let parsed_date = NaiveDateTime::parse_from_str(date_string, format);
+
+    match parsed_date {
+        Ok(parsed) => Some(Local.from_local_datetime(&parsed).single().unwrap()),
+        Err(_) => None,
+    }
+}
+
+fn format_date(date: DateTime<Local>, pattern: &str) -> String {
+    date.format(pattern).to_string()
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
+    let pattern = "%Y%m%d_%H%M%S"; // Example pattern: YYYYMMDD_hh24mmss
     
     if args.len() != 2 {
         println!("Usage: exif_renamer <directory>");
@@ -30,11 +47,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let mut bufreader = std::io::BufReader::new(&filex);
                 let exifreader = exif::Reader::new();
                 let exif = exifreader.read_from_container(&mut bufreader)?;
+
                 println!("filename: {}", path.display());
                 for f in exif.fields() {
                     if f.tag.to_string().to_lowercase().starts_with("date") {
+                      let date_string = f.display_value().to_string();
                       println!("{} {} {}",
-                              f.tag, f.ifd_num, f.display_value().with_unit(&exif));
+                              f.tag, f.ifd_num, date_string);
+                      let current_date  = create_datetime_from_string(&date_string);
+                      let formatted_date = format_date(current_date.unwrap(), pattern);
+                      println!("Formatted date: {}", formatted_date);
                     }
                 }
                 println!("")
