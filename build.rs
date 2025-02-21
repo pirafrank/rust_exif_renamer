e9a32bdb7ef12ca3c71c639c15eed02ae6fc6f4e
Francesco Pira <dev@fpira.com>
2025-02-21 23:45:39 +0100

    feat: show build info in version info

---
+++ build.rs
@@ -0,0 +1,20 @@
+use std::process::Command;
+use chrono::Utc;
+
+fn main() {
+    // Get the short commit hash
+    let output = Command::new("git")
+        .args(&["rev-parse", "HEAD"])
+        .output()
+        .expect("Failed to execute git command");
+
+    let git_hash = String::from_utf8(output.stdout).expect("Invalid UTF-8 sequence");
+
+    // today date
+    let now = Utc::now();
+    let build_date = now.format("%Y-%m-%d %H:%M:%S UTC").to_string();
+
+    // Set the environment variables
+    println!("cargo:rustc-env=GIT_COMMIT_HASH={}", git_hash.trim());
+    println!("cargo:rustc-env=BUILD_DATE={}", build_date);
+}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,29 @@ use std::fs;
 mod exif;
 mod exif_error;
 
+// Version constants
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const COMMIT: &str = env!("GIT_COMMIT_HASH");
+const BUILD_DATE: &str = env!("BUILD_DATE");
+const AUTHOR : &str = env!("CARGO_PKG_AUTHORS");
+const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+
+fn long_version() -> &'static str {
+    Box::leak(
+        format!(
+            "Version: {}\nCommit: {}\nBuild Date: {}",
+            VERSION, COMMIT, BUILD_DATE
+        )
+        .into_boxed_str()
+    )
+}
+
+// Constants
+
 const DEFAULT_PATTERN: &str = "%Y%m%d_%H%M%S";
+
+// Command line interface
 
 #[derive(Clone, ValueEnum)]
 enum Cmd {
@@ -17,7 +39,12 @@ enum Cmd {
 }
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(
+  author = AUTHOR,
+  version = VERSION,
+  about = DESCRIPTION,
+  long_version = long_version()
+)]
 struct Cli {
     /// Command to execute
     command: Cmd,
@@ -30,6 +57,8 @@ struct Cli {
     #[arg(short = 'P', long, default_value = DEFAULT_PATTERN)]
     pattern: Option<String>,
 }
+
+// main
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
